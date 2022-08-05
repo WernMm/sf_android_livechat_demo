1cc25c1a015566b16330d3adad1320ff1b7ba4cb
Somwut Jamroonsilp <somwut.j@beryl8.com>
2022-08-05 14:32:45 +0700

    test android13

--- app/src/main/java/com/example/sflivechat/MainActivity.java
+++ app/src/main/java/com/example/sflivechat/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
 import com.salesforce.android.chat.core.ChatConfiguration;
 import com.salesforce.android.chat.ui.ChatUI;
@@ -11,13 +12,21 @@ import com.salesforce.android.chat.ui.ChatUIClient;
 import com.salesforce.android.chat.ui.ChatUIConfiguration;
 import com.salesforce.android.service.common.utilities.control.Async;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
+    // DEV
     public static final String ORG_ID = "00DO000000544EH";
     public static final String DEPLOYMENT_ID = "572O000000000bk";
-    public static final String BUTTON_ID = "573O000000000dg";
+    public static final String BUTTON_ID = "573O000000000ey";
     public static final String LIVE_AGENT_POD = "d.la1-c1cs-ukb.salesforceliveagent.com";
+
+    //UAT
+//    public static final String ORG_ID = "00D0w0000000aS5";
+//    public static final String DEPLOYMENT_ID = "572O000000000bk";
+//    public static final String BUTTON_ID = "573O000000000ey";
+//    public static final String LIVE_AGENT_POD = "d.la1-c1cs-ukb.salesforceliveagent.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +34,18 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_main);
         ChatConfiguration.Builder chatConfigBuilder = new ChatConfiguration.Builder(ORG_ID, BUTTON_ID, DEPLOYMENT_ID, LIVE_AGENT_POD);
 
-
         ChatUIConfiguration.Builder chatUIConfigurationBuilder = new ChatUIConfiguration.Builder();
         chatUIConfigurationBuilder.defaultToMinimized(false);
         chatUIConfigurationBuilder.chatConfiguration(chatConfigBuilder.build());
+
+//        binding.salesforceMessageInputActionButton2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                launchChatViaWebView(view);
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
 
         ChatUI.configure(chatUIConfigurationBuilder.build())
                 .createClient(getApplicationContext())
